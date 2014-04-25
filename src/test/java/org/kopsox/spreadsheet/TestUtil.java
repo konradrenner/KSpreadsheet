01e4489a1f7ccd8d43ebe1a037f1d131aa2f1ddf
Konrad Renner <koni.renner@gmail.com>
2014-04-25 16:42:24 +0200

    Test Ressources angepasst

--- src/test/java/org/kopsox/spreadsheet/TestUtil.java
+++ src/test/java/org/kopsox/spreadsheet/TestUtil.java
@@ -1,13 +1,14 @@
 package org.kopsox.spreadsheet;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
+import org.junit.Assert;
 
 public class TestUtil {
 
 
     public static InputStream getSpreadSheetStrean(String filename) throws Exception {
-        return new FileInputStream(new File(System.getProperty("path_to_spreadsheets") + filename));
+        InputStream resourceAsStream = TestUtil.class.getResourceAsStream(filename);
+        Assert.assertNotNull(resourceAsStream);
+        return resourceAsStream;
     }
 }
